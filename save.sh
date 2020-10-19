f5d655f5ef48406b94be61b0ca82cb3c4f1fc138
Christophe Demottie <christophe.demottie@orange.com>
2020-10-19 15:46:41 +0200

    add component to save

--- save.sh
+++ save.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar czf ../door_manage.tgz wifi_control.sh simulator/sgpiozero.py simulator/__init__.py elements/wifi_management.py elements/__init__.py elements/automatic_door.py elements/advanced_elements.py door_management.sh door_management.py chicken.json door_daemon.sh
+tar czf ../door_manage.tgz wifi_control.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken.json door_daemon.sh
 
