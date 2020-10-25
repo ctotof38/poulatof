8fb9f1016595a8df3a2bad9e8a91afdca8211041
Christophe Demottie <christophe.demottie@orange.com>
2020-10-25 07:24:40 +0100

    manage empty email info

--- elements/email_sender.py
+++ elements/email_sender.py
@@ -3,21 +3,30 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
 import threading
+import logging
+
+mail_logger = logging.getLogger('email_sender')
 
 
 # object ready to send an email
 # if password_mail is not included in configuration, used default configured in this file
+# logger used to get current log and rotate
 class EmailSender:
     def __init__(self, configuration, logger):
         self.logger = logger
         self.configuration = configuration
         self.no_active = False
         try:
-            # mandatory field to activate service
-            configuration["user_mail"]
-            configuration["destination_mail"]
+            # mandatory fields to activate service
+            user = configuration["user_mail"]
+            if len(user) == 0:
+                raise KeyError
+            destination = configuration["destination_mail"]
+            if len(destination) == 0:
+                raise KeyError
         except KeyError:
-            # no mandatory field, so, email not active
+            # no mandatory fields, so, email not active
+            mail_logger.info("service not active, no enough configuration")
             self.no_active = True
 
     def send(self):
@@ -35,6 +44,8 @@ class EmailSender:
 
         try:
             password = self.configuration["password_mail"]
+            if len(password) == 0:
+                raise KeyError
         except KeyError:
             password = "gmail_appli_passwd"
 
