0d2f7a021f576464551c6a19cbf1127ef08ac33c
Christophe Demottie <christophe.demottie@orange.com>
2020-10-25 07:27:04 +0100

    improve save

--- save.sh
+++ save.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar czf ../door_manage.tgz wifi_control.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken.json door_daemon.sh
+tar czf ../door_manage.tgz wifi_control.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken*.json door_daemon.sh
 
