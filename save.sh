0afd531e9e543861936e935e5753a3e275e0ca6c
Christophe Demottie <christophe.demottie@orange.com>
2020-11-23 15:57:06 +0100

    add control

--- save.sh
+++ save.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-tar czf ../door_manage.tgz wifi_control.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken*.json door_daemon.sh
+FILE=../door_manage.tgz
+
+rm -f $FILE
+tar czf $FILE wifi_control.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken*.json door_daemon.sh
+if [ $? -eq 0 ]
+then
+	echo "$FILE generated"
+else
+	echo "problem to create $FILE"
+fi
 
