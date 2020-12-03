28424e4412448e7a5c6ab1ff97355afd5abeb586
Christophe Demottie <christophe.demottie@orange.com>
2020-12-03 09:40:00 +0100

    save watchdog file

--- save.sh
+++ save.sh
@@ -3,7 +3,7 @@
 FILE=../door_manage.tgz
 
 rm -f $FILE
-tar czf $FILE wifi_control.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken*.json door_daemon.sh
+tar czf $FILE wifi_control.sh watchdog.sh simulator/*.py elements/*.py door_management.sh door_management.py chicken*.json door_daemon.sh
 if [ $? -eq 0 ]
 then
 	echo "$FILE generated"
